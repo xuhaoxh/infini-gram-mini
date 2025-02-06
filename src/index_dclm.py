cc8acc6ae86c8aaae1a105249e875f8b82cb1a59
Jiacheng Liu <liujch1998@gmail.com>
2025-02-06 03:02:07 +0000

    One file per shard

--- src/index_dclm.py
+++ src/index_dclm.py
@@ -2,25 +2,21 @@ import os
 import sys
 import zstandard as zstd
 
-TOTAL_FILES = 1970
-NUM_FILES_PER_SHARD = int(sys.argv[1])
-NUM_SHARDS = int(sys.argv[2])
+SHARD_IX = int(sys.argv[1])
 input_dir = '/weka/oe-data-default/ai2-llm/pretraining-data/sources/olmo-mix/olmoe-mix-0924/data/dclm'
-output_dir = f'/weka/oe-training-default/jiachengl/hf-fm/dclm_f{NUM_FILES_PER_SHARD}_s{NUM_SHARDS}'
+output_dir = f'/weka/oe-training-default/jiachengl/hf-fm/dclm_f1_all'
 
 os.makedirs(output_dir, exist_ok=True)
 
-for s in range(NUM_SHARDS):
-    raw_path = f'{output_dir}/data_{s:02d}.jsonl'
-    f_raw = open(raw_path, 'w')
-    for i in range(s * NUM_FILES_PER_SHARD, min((s + 1) * NUM_FILES_PER_SHARD, TOTAL_FILES)):
-        input_path = f'{input_dir}/dclm-{i:04d}.json.zst'
-        with open(input_path, 'rb') as f:
-            dctx = zstd.ZstdDecompressor()
-            with dctx.stream_reader(f) as reader:
-                decompressed_data = reader.read().decode('utf-8')
-        f_raw.write(decompressed_data.strip('\n') + '\n')
-    f_raw.close()
-    log = os.popen(f'./construct {raw_path} {output_dir}/').read()
-    print(log)
-    os.remove(raw_path)
+raw_path = f'{output_dir}/data_{SHARD_IX:04d}.jsonl'
+f_raw = open(raw_path, 'w')
+input_path = f'{input_dir}/dclm-{SHARD_IX:04d}.json.zst'
+with open(input_path, 'rb') as f:
+    dctx = zstd.ZstdDecompressor()
+    with dctx.stream_reader(f) as reader:
+        decompressed_data = reader.read().decode('utf-8')
+f_raw.write(decompressed_data.strip('\n') + '\n')
+f_raw.close()
+log = os.popen(f'./construct {raw_path} {output_dir}/').read()
+print(log)
+os.remove(raw_path)
--- src/index_dclm.sh
+++ src/index_dclm.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-RUN_NAME="fm_dclm_f20_s1"
+SHARD_IX=$1
+RUN_NAME="fm_dclm_f1_${SHARD_IX}"
 
 gantry run \
   --allow-dirty \
@@ -19,7 +20,7 @@ gantry run \
   --weka oe-training-default:/weka/oe-training-default \
   --weka oe-data-default:/weka/oe-data-default \
   --cpus 4 \
-  --memory 1912GiB \
+  --memory 95GiB \
   --shared-memory 10GiB \
   --env-secret GITHUB_TOKEN=GITHUB_TOKEN_HF \
   --no-python \
@@ -32,5 +33,5 @@ gantry run \
     pip install zstandard; \
     cd src; \
     g++ -std=c++17 -I../sdsl/include -L../sdsl/lib construct.cpp -o construct -lsdsl -ldivsufsort -ldivsufsort64; \
-    python index_dclm.py 20 1; \
+    python index_dclm.py ${SHARD_IX}; \
     "
