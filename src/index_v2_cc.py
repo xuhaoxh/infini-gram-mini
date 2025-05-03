6d9dd075893f6cf19547d6cabbe311c187a15811
Ubuntu <ubuntu@ip-172-31-42-74.ec2.internal>
2025-05-03 18:22:45 +0000

    Index CC-2025-08

--- src/index_v2_cc.py
+++ src/index_v2_cc.py
@@ -1,5 +1,6 @@
 import argparse
 import multiprocessing as mp
+import numpy as np
 import os
 import resource
 import shutil
@@ -10,17 +11,34 @@ from indexing import prepare, build_sa_bwt
 
 def main():
 
-    for s in range(15):
-        save_dir = f'/weka/oe-training-default/jiachengl/hf-fm/index/v2_cc/2025-05/{s:02d}'
-        if os.path.exists(save_dir):
-            print(f'Index shard {s:02d} already exists. Skipping.', flush=True)
-            continue
-        print(f'Index shard {s:02d} does not exist. Creating ...', flush=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--crawl_name', type=str, required=True)
+    parser.add_argument('--shards_per_shard', type=int, default=7)
+    parser.add_argument('--doc_sep', type=bytes, default=b'\xff')
+    parser.add_argument('--batch_size', type=int, default=65536, help='Batch size for prepare.')
+    parser.add_argument('--cpus', type=int, default=mp.cpu_count(), help='Number of CPU cores available to the program.')
+    parser.add_argument('--mem', type=int, required=True, help='Amount of memory in GiB available to the program.')
+    parser.add_argument('--ulimit', type=int, default=1048576, help='Maximum number of open files allowed.')
+    args = parser.parse_args()
+
+    assert args.batch_size > 0
+    assert args.cpus > 0
+
+    assert sys.byteorder == 'little'
+    resource.setrlimit(resource.RLIMIT_NOFILE, (args.ulimit, args.ulimit))
+
+    args.num_shards = int(np.ceil(100 / args.shards_per_shard))
+    for s in range(args.num_shards):
+        save_dir = f'/data_t/v2_cc-{args.crawl_name}/{s:02d}'
         os.makedirs(save_dir, exist_ok=True)
-        data_dir = os.path.join(os.getcwd(), f'cc_s{s:02d}') # this is an ephemeral directory
+        data_dir = f'/data_c/cc-{args.crawl_name}/{s:02d}'
         os.makedirs(data_dir, exist_ok=True)
 
-        remote_data_dir = 's3://ai2-llm/pretraining-data/sources/dclm/pool/documents/CC-MAIN-2025-05/segments'
+        args.data_dir = data_dir
+        args.save_dir = save_dir
+        args.temp_dir = save_dir
+
+        remote_data_dir = f's3://ai2-llm/pretraining-data/sources/dclm/pool/documents/CC-MAIN-{args.crawl_name}/segments'
         output = os.popen(f'aws s3 ls {remote_data_dir}/').read()
         lines = output.split('\n')
         lines = [line for line in lines if 'PRE ' in line]
@@ -28,7 +46,7 @@ def main():
         subdirs = sorted(subdirs, key=lambda x: int(x.split('.')[-1]))
         assert len(subdirs) == 100
 
-        for shard_ix in tqdm(list(range(s * 7, min((s + 1) * 7, 100)))):
+        for shard_ix in tqdm(list(range(s * args.shards_per_shard, min((s + 1) * args.shards_per_shard, 100)))):
             subdir = subdirs[shard_ix]
             print(f'Downloading data from s3: subdir={subdir}', flush=True)
             dir = f'{data_dir}/{subdir}'
@@ -37,29 +55,13 @@ def main():
             process.wait()
             print(f'Downloaded. Total size: {os.popen(f"du -sh {dir}").read()}', flush=True)
 
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--doc_sep', type=bytes, default=b'\xff')
-        parser.add_argument('--batch_size', type=int, default=65536, help='Batch size for prepare.')
-        parser.add_argument('--cpus', type=int, default=mp.cpu_count(), help='Number of CPU cores available to the program.')
-        parser.add_argument('--mem', type=int, required=True, help='Amount of memory in GiB available to the program.')
-        parser.add_argument('--ulimit', type=int, default=1048576, help='Maximum number of open files allowed.')
-        args = parser.parse_args()
-        args.data_dir = data_dir
-        args.save_dir = save_dir
-        args.temp_dir = args.save_dir
-
-        assert args.batch_size > 0
-        assert args.cpus > 0
-
-        assert sys.byteorder == 'little'
-        resource.setrlimit(resource.RLIMIT_NOFILE, (args.ulimit, args.ulimit))
-
         prepare(args)
         build_sa_bwt(args, mode='data')
         build_sa_bwt(args, mode='meta')
         print(os.popen(f'./cpp_indexing {args.save_dir} 2>/dev/null').read(), flush=True)
 
         shutil.rmtree(data_dir)
+        shutil.move(save_dir, f'/data_i/v2_cc-{args.crawl_name}/{s:02d}')
 
 if __name__ == '__main__':
     main()
